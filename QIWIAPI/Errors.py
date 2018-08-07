aaa974dc11016b57d3bc3d115a11984ed0e39ec8
Mr.Stoner <34810078+stoner1776@users.noreply.github.com>
2018-08-07 04:34:53 +0300

    Create Errors.py

---
+++ QIWIAPI/Errors.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class QIWIAPIError(Exception):
+    pass
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class InvalidTokenError(Exception):
+    pass
+
+
+class OverridingEx(Exception):
+    pass
