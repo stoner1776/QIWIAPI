15fa2921de0d3c6802e18ba433c73fb3c3961b3e
Mr.Stoner <34810078+stoner1776@users.noreply.github.com>
2018-08-07 04:42:13 +0300

    v.3

---
+++ QIWIAPI/__version__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Written by: * StoNeR 1776
+# https://github.com/stoner1776/QIWIAPI/
+
+from QIWIAPI.Errors import *
+from QIWIAPI.Session import *
+from QIWIAPI.__version__ import *
